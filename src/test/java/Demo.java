6c20b0481fb0e3311c863b2e2c9f70982aee0486
Burak <90210682+burak-qa@users.noreply.github.com>
2024-05-22 13:21:36 +0200

    Update Demo.java

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,12 +1,28 @@
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+
+import java.nio.file.Paths;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class Demo {
     public static void main(String[] args){
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
 
-        Page page  = browser.newPage();
+
+        BrowserContext context = browser.newContext();
+        context.tracing().start(new Tracing.StartOptions()
+                .setScreenshots(true)
+                .setSnapshots(true)
+                .setSources(true));
+
+
+        Page page  = context.newPage();
         page.navigate("https://www.google.com/");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Alles accepteren")).click();
+        assertThat(page.getByRole(AriaRole.IMG, new Page.GetByRoleOptions().setName("Google"))).isVisible();
+        System.out.println(page.title());
         System.out.println("Hello playwright");
     }
 }
